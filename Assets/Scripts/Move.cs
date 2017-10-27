2fcfd6c44dbf741af99f5d7e27a0cb0ebe90ccef
Adina Țeudan <adina.teudan@gmail.com>
2017-10-27 16:24:09 +0300

    first

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        speed += 0.00001f;
+        float moveForward = 1.0f;
+        float moveSideways = Input.GetAxis("Horizontal") * 3;
+
+        Vector3 movement = new Vector3(moveSideways, 0.0f, moveForward * speed);
+
+        rb.AddForce(movement);
+    }
+}
